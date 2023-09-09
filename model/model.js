2ad1c9bc62a4190985f76431d6c4fa99d92c4d10
ProXyTG <proxytg1@gmail.com>
2023-09-09 13:27:39 +0200

    refactor: :recycle: restructure model
    restructure model

--- model/model.js
+++ model/model.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
-    name: {
-        required: true,
-        type: String
-    },
-    age: {
-        required: true,
-        type: Number
-    }
+  name: {
+    required: true,
+    type: String
+  },
+  description: {
+    required: true,
+    type: String
+  },
+  location: {
+    required: true,
+    type: String
+  },
+  date: {
+    required: true,
+    type: String
+  }
 })
 
 module.exports = mongoose.model('Data', dataSchema)
